19fc14a34c3cb2c36f0e3a25a523b566c458a817
MosesChisz <mosesc@cortexware.co.za>
2024-08-02 15:25:08 +0200

    create get user listings api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteteUser, test, updateUser } from '../controllers/user.controller.js';
+import { deleteteUser, test, updateUser, getUserListings } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteteUser);
+router.get('/listings/:id', verifyToken, getUserListings);
 
 export default router;
